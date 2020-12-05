8e35c16d83e924e6e9fad8edc88df315df8f8be2
IrvingW <irvingwang7@gmail.com>
2020-12-06 01:07:10 +0800

    add benmark test for checkwebsites func

--- basics/concurrency/check_website_benchmark_test.go
+++ basics/concurrency/check_website_benchmark_test.go
@@ -1,1 +1,21 @@
 package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+func slowWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Microsecond)
+	return false
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a demo url"
+	}
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowWebsiteChecker, urls)
+	}
+}
