e895553cc08d3898db0f14a46d6ea1ab8ed450aa
IrvingW <irvingwang7@gmail.com>
2020-12-06 17:58:47 +0800

    add timeout config

--- basics/select/racer.go
+++ basics/select/racer.go
@@ -2,16 +2,31 @@ package racer
 
 import (
 	"net/http"
+	"time"
+)
+
+// HTTPError 自定义Error
+type HTTPError string
+
+func (e HTTPError) Error() string {
+	return string(e)
+}
+
+const (
+	// TimeoutError 当服务器超时时返回
+	TimeoutError = HTTPError("This url has timeout.")
 )
 
 // Racer 返回两个url中返回快的那个
-func Racer(a, b string) string {
+func Racer(a, b string, timeout time.Duration) (string, error) {
 	// select 会同时监听两个channel
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", TimeoutError
 	}
 }
 
--- basics/select/racer_test.go
+++ basics/select/racer_test.go
@@ -8,17 +8,37 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeMockServer(t, 1*time.Second)
-	fastServer := makeMockServer(t, 0)
-	// 在某个函数调用前加上 defer 前缀会在 包含它的函数结束时 调用它
-	defer slowServer.Close()
-	defer fastServer.Close()
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-	winner := Racer(slowURL, fastURL)
-	if winner != fastURL {
-		t.Errorf("got %v but want %v", winner, fastURL)
-	}
+	t.Run("normal case", func(t *testing.T) {
+		slowServer := makeMockServer(t, 1*time.Second)
+		fastServer := makeMockServer(t, 0)
+		// 在某个函数调用前加上 defer 前缀会在 包含它的函数结束时 调用它
+		defer slowServer.Close()
+		defer fastServer.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		winner, err := Racer(slowURL, fastURL, 10*time.Second)
+		if err != nil {
+			t.Fatalf("got an error %v", err)
+		}
+		if winner != fastURL {
+			t.Errorf("got %v but want %v", winner, fastURL)
+		}
+	})
+
+	t.Run("when server will timeout", func(t *testing.T) {
+		slowServer := makeMockServer(t, 11*time.Second)
+		fastServer := makeMockServer(t, 10*time.Second)
+		// 在某个函数调用前加上 defer 前缀会在 包含它的函数结束时 调用它
+		defer slowServer.Close()
+		defer fastServer.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		_, err := Racer(slowURL, fastURL, 10*time.Second)
+		if err != TimeoutError {
+			t.Errorf("want error %s but got error %s", TimeoutError, err)
+		}
+
+	})
 }
 
 func makeMockServer(t *testing.T, delay time.Duration) *httptest.Server {
